3c5f868cede98bef7c7e0d2135989cb62796992d
Rikku <rikkubook@gmail.com>
2022-05-01 18:11:31 +0800

    修改 posts 添加文章

--- models/posts.js
+++ models/posts.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
-const postSchema = new mongoose.Schema(
+const postSchema = new mongoose.Schema({
   /* 加入欄位驗證 */
+  name: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: [true, '貼文內容未填寫']
+  },
+  img: {
+    type: String,
+    default: ''
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+    select: false
+  },
+},
+  {
+    versionKey: false
+  }
 );
 
 const Post = mongoose.model('Post', postSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -4,16 +4,20 @@ const Post = require('../models/posts');
 
 router.post('/', async(req, res, next) =>  {
   try {
-      /* 請在此填寫答案 */
-      // 取得來自 request body 的資料
-      
-      // 驗證是否有 content 欄位 -> 若有則使用 mongoose 語法新增資料 -> 回傳成功回應
-      //                       -> 未填寫 content 欄位 -> 回傳失敗回應
-  
+    const {name, content, img} = req.body
+    if(!content){
+      throw "貼文內容未填寫"
+    }
+    const newPost = await Post.create({name, content, img})
+    res.status(200).json({
+      status: "success",
+      data: newPost
+    })
   } catch (error) {
+
     res.status(400).json({
         status: 'false',
-        "message": "欄位未填寫正確，或無此 todo ID"
+        message: error  || "欄位未填寫正確，或無此 todo ID"
     });
   }
 })
